44da0692fc977240ce35cdd28087f50082147fc3
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2021-11-05 16:06:37 +0900

    ✨ Update example props

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -4,11 +4,13 @@ import UrlImageBlurryLazyLoad from '../../src/index';
 
 const App = () => (
   <UrlImageBlurryLazyLoad
-    thumbnailImageUrl={'https://i.postimg.cc/2yKZMcHH/thumbnail-IMG-4797-5e394d3c6e.jpg'}
+    thumbnailImageUrl={'https://raw.githubusercontent.com/zhumeisongsong/react-url-image-blurry-lazy-load/main/example/images/thumbnail_IMG_4797_5e394d3c6e.jpeg'}
     imageUrl={
-      'https://i.postimg.cc/Jhj2KY3m/IMG-4797.jpg'
+      'https://raw.githubusercontent.com/zhumeisongsong/react-url-image-blurry-lazy-load/main/example/images/IMG_4797.jpeg'
     }
-    imageAlt={''}
+    className={''}
+    imageHeight={'66.6%'}
+    imageAlt={'text lazy load'}
   />
 );
 render(<App />, document.getElementById('root'));
